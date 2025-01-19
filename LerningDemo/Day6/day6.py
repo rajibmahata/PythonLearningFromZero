711bb325af16f32c1077d78f80d0fcde82cf3655
rajibmahata <rajibmahata143@outlook.com>
2025-01-19 17:32:27 +0530

    added days 6

--- LerningDemo/Day6/day6.py
+++ LerningDemo/Day6/day6.py
@@ -48,4 +48,49 @@ print(set1)
 
 
 #functions :
+print("Functions")
 
+def add(a,b,c):
+    return a+b+c
+
+print(add(1,2,3))
+
+
+def add(a,b,c=0):
+    return a+b+c    
+
+print(add(1,2))
+
+def GetName(name):
+    return name
+
+print(GetName("Rajib"))
+
+def GetAge(age=0):
+    return age
+
+print(GetAge(25))
+
+function = lambda a,b,c : a+b+c
+
+print(function(1,2,3))
+
+
+def sum(*args):
+    total=0
+    for a in args:
+        total+=a
+    return total
+
+print(sum(1,2,3,4,5))
+
+
+#lambda function with *args
+
+sum = lambda *args: sum(args)
+
+print(sum(1,2,3,4,5))
+
+squere= lambda x: x*x
+
+print(squere(10))
---
+++ LerningDemo/Day6/lambdaFunction.py
@@ -0,0 +1,9 @@
+#lambda function with *args
+
+sum = lambda *args: sum(args)
+
+print(sum(1,2,3,4,5))
+
+squere= lambda x: x*x
+
+print(squere(10))
